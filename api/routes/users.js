1b2f93eb96c1af81008e89a894baf45eb7a53d57
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2023-07-24 22:39:55 -0400

    forced audit lol

--- api/routes/users.js
+++ api/routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
     res.send('respond with a resource');
 });
 
+router.get('/getUser', function(req, res, next) {
+    res.send('Respond with user');
+});
+
+router.post('/createUser', function(req, res, next) {
+    res.send('POST to create user');
+});
+
 module.exports = router;
